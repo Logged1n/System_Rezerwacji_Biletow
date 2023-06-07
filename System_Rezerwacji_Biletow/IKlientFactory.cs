0ca43295a4b5ec52ac50353a00ce17b58b316a73
JacobWick <134642281+JacobWick@users.noreply.github.com>
2023-06-07 16:06:26 +0200

    Fabryka Klientów

---
+++ System_Rezerwacji_Biletow/IKlientFactory.cs
@@ -0,0 +1,6 @@
+namespace System_Rezerwacji_Biletow;
+
+public interface IKlientFactory
+{
+    public Klient CreateKlient();
+}
--- System_Rezerwacji_Biletow/Klient.cs
+++ System_Rezerwacji_Biletow/Klient.cs
@@ -1,6 +1,9 @@
 namespace System_Rezerwacji_Biletow;
 
-public class Klient
+public abstract class Klient
 {
     public string Id { get; protected set; } // przyklad jak masz deklarowac pola. get bez niczego znaczy ze ma ta sama hermetyzacje co pole (czyli tutaj public), wiec dowolna klasa moze je bezposrednio odczytac, bez oddzielnej funkcji. protected set; znaczy ze tylko klasy dziedziczace z tej klasy moga ustawiac wartosci na tym polu.
+    public string NumerTelefonu { get; protected set;}
+    public string Email {get; protected set;}
+    
 }
