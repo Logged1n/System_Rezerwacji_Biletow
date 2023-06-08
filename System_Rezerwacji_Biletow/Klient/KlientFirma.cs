cea0f8c66f9e3e96ae0e830a6045f434224f00f1
Lukasz Halicki <uhalicki@gmail.com>
2023-06-08 23:43:27 +0200

    Comments

--- System_Rezerwacji_Biletow/Klient/KlientFirma.cs
+++ System_Rezerwacji_Biletow/Klient/KlientFirma.cs
@@ -4,7 +4,7 @@ public class KlientFirma : Klient
 {
     public string NazwaFirmy { get; }
 
-    public KlientFirma(string id, string numerTelefonu, string email, string nazwaFirmy) : base(id, numerTelefonu, email)
+    public KlientFirma(string id, string numerTelefonu, string email, string nazwaFirmy) : base(id, numerTelefonu, email) // jak zrobisz KlientManagement to w base id zamien na cos podobnego co jest w samolotach
     {
         NazwaFirmy = nazwaFirmy;
     }
--- System_Rezerwacji_Biletow/Samolot/Samolot.cs
+++ System_Rezerwacji_Biletow/Samolot/Samolot.cs
@@ -6,17 +6,17 @@ namespace System_Rezerwacji_Biletow;
 public abstract class Samolot
 {
     //TODO klasy dziedziczace, fabryki, ewentualnie w interface dodac argumenty CreateSamolot()
-    public string Id { get; protected set; }
-    public int IloscMiejsc { get; protected set; }
-    public int Zasieg { get; protected set; }
-    public Lotnisko PoczatkoweLotnisko { get; protected set; }
+    public string Id { get; }
+    public int IloscMiejsc { get; }
+    public int Zasieg { get; }
+    public Lotnisko PoczatkoweLotnisko { get; }
     
-    protected Samolot(string id, int iloscMiejsc, int zasieg, Lotnisko lotnisko)
+    protected Samolot(string id, int iloscMiejsc, int zasieg, Lotnisko poczatkoweLotnisko)
     {
         Id = id;
         IloscMiejsc = iloscMiejsc;
         Zasieg = zasieg;
-        PoczatkoweLotnisko = lotnisko;
+        PoczatkoweLotnisko = poczatkoweLotnisko;
     }
 
     public override string ToString()
