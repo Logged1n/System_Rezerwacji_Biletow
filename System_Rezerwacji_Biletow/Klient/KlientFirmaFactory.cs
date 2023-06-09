6ae5f925edc1c7df0aeb0a81454af9a9af9ddc25
JacobWick <134642281+JacobWick@users.noreply.github.com>
2023-06-09 11:10:06 +0200

    konstruktor do rezerwacji, firmafactory poprawione

--- System_Rezerwacji_Biletow/Klient/KlientFirmaFactory.cs
+++ System_Rezerwacji_Biletow/Klient/KlientFirmaFactory.cs
@@ -2,8 +2,8 @@ namespace System_Rezerwacji_Biletow;
 
 public class KlientFirmaFactory: IKlientFactory
 {
-    public Klient CreateKlient(string id, string numerTelefonu, string email, string dodatkowePole1, string dodatkowePole2 = null)
+    public Klient CreateKlient(string id, string numerTelefonu, string email, string firma, string dodatkowePole2 = null)
     {
-        return new KlientFirma(id, numerTelefonu, email, dodatkowePole1);
+        return new KlientFirma(id, numerTelefonu, email, firma);
     }
 }
--- System_Rezerwacji_Biletow/SimpleObjects/Rezerwacja.cs
+++ System_Rezerwacji_Biletow/SimpleObjects/Rezerwacja.cs
@@ -6,5 +6,12 @@ public class Rezerwacja
     public string Id { get; }
     public Klient Klient { get; }
     public Lot.Lot Lot { get; }
+
+    public Rezerwacja(string id, Klient klient, Lot.Lot lot)
+    {
+        Id = id;
+        Klient = klient;
+        Lot = lot;
+    }
 }
    
