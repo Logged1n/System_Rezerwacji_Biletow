4c0642f569a22781f4a8a2b04b15b72e670fda93
Dawid Davtyan <dawiddavtyan16@gmail.com>
2023-06-10 14:19:49 +0200

    Samolot i TrasaManagment Testy

---
+++ System_Rezerwacji_Biletow.Tests/SamolotManagementTests.cs
@@ -0,0 +1,63 @@
+using NUnit.Framework;
+namespace System_Rezerwacji_Biletow.Tests;
+using Managements;
+using Samolot;
+
+[TestFixture]
+public class SamolotManagementTests
+{
+    private SamolotManagement _management = SamolotManagement.GetInstance();
+    [SetUp]
+    public void SetUp()
+    {
+        _management.Reset();
+    }
+    [Test]
+    public void DodajTest()
+    {
+        //Arrange
+        SamolotRegionalnyFactory SRF = new SamolotRegionalnyFactory();
+        SamolotSzerokokadlubowyFactory SSF = new SamolotSzerokokadlubowyFactory();
+        SamolotWaskokadlubowyFactory SWF = new SamolotWaskokadlubowyFactory();
+        Lotnisko obj = new Lotnisko("TestKraj", "TestMiasto", "TestNazwa");
+        Samolot samolot = SRF.CreateSamolot(obj);
+        Samolot samolot1 = SSF.CreateSamolot(obj);
+        Samolot samolot2 = SWF.CreateSamolot(obj);
+        
+        //Act
+        _management.Dodaj(samolot);
+        _management.Dodaj(samolot1);
+        _management.Dodaj(samolot2);
+        
+        //Assert
+        Assert.Contains(samolot, _management.GetList());
+        Assert.Contains(samolot1,_management.GetList());
+        Assert.Contains(samolot2,_management.GetList());
+    }
+
+    [Test]
+    public void UsunTest()
+    {
+        //Arrange
+        SamolotRegionalnyFactory SRF = new SamolotRegionalnyFactory();
+        SamolotSzerokokadlubowyFactory SSF = new SamolotSzerokokadlubowyFactory();
+        SamolotWaskokadlubowyFactory SWF = new SamolotWaskokadlubowyFactory();
+        Lotnisko obj = new Lotnisko("TestKraj", "TestMiasto", "TestNazwa");
+        Samolot samolot = SRF.CreateSamolot(obj);
+        Samolot samolot1 = SSF.CreateSamolot(obj);
+        Samolot samolot2 = SWF.CreateSamolot(obj);
+        _management.Dodaj(samolot);
+        _management.Dodaj(samolot1);
+        _management.Dodaj(samolot2);
+        
+        //Act
+        _management.Usun(samolot);
+        _management.Usun(samolot1);
+        _management.Usun(samolot2);
+        
+        //Assert
+        Assert.IsFalse(_management.GetList().Contains(samolot));
+        Assert.IsFalse(_management.GetList().Contains(samolot1));
+        Assert.IsFalse(_management.GetList().Contains(samolot2));
+    }
+}
---
+++ System_Rezerwacji_Biletow.Tests/TrasaManagementTests.cs
@@ -0,0 +1,46 @@
+namespace System_Rezerwacji_Biletow.Tests;
+using NUnit.Framework;
+using Managements;
+
+[TestFixture]
+public class TrasaManagementTests
+{
+    private TrasaManagement _management = TrasaManagement.GetInstance();
+    
+    [SetUp]
+    public void SetUp()
+    {
+        _management.Reset();
+    }
+
+    [Test]
+    public void DodajTest()
+    {
+        //Arrange
+        Lotnisko lot1 = new Lotnisko("TestKraj", "TestMiasto", "TestNazwa");
+        Lotnisko lot2 = new Lotnisko("TestKraj", "TestMiasto", "TestNazwa");
+        Trasa obj = new Trasa("0000", lot1, lot2,100);
+        
+        //Act
+        _management.Dodaj(obj);
+        
+        //Assert
+        Assert.Contains(obj, _management.GetList());
+    }
+    
+    [Test]
+    public void UsunTest()
+    {
+        //Arrange
+        Lotnisko lot1 = new Lotnisko("TestKraj", "TestMiasto", "TestNazwa");
+        Lotnisko lot2 = new Lotnisko("TestKraj", "TestMiasto", "TestNazwa");
+        Trasa obj = new Trasa("0000", lot1, lot2,100);
+        _management.Dodaj(obj);
+        
+        //Act
+        _management.Usun(obj);
+        
+        //Assert
+        Assert.IsFalse(_management.GetList().Contains(obj));
+    }
+}
